92d3ecd42fe0a91bfaf8e1e4d7fc981deef0b94f
Adam Smith <adam.mackenzie.smith@gmail.com>
2013-06-01 17:21:21 +1000

    extracted article fetch method

--- guess-question/main.rb
+++ guess-question/main.rb
@@ -6,38 +6,41 @@ set :port, 9002
 get '/question.json' do
     cache_control :no_cache
     content_type "application/json", :charset => 'utf-8'
-    articles = [
-      { 
-        lines: [
-          { 
-            words: [
-              {
-                id: "asdkjhg",
-                image: "http://localhost:9001/word/asdkjhg.jpg"
-              },          
-              {
-                id: "00001",
-                image: "http://localhost:9001/word/00001.jpg"
-              }
-
-            ]
-          }
-        ]
-      }
-    ]
-
+    
+    articles = fetch_articles
     article = random_element(articles)
     line = random_element(article[:lines])
     word = random_element(line[:words])
 
-    question = {
+    {
       id: word[:id],
       image: word[:image]
-    }
-    question.to_json
+    }.to_json
 end
 
 def random_element(list)
   index = Random.rand(list.size)
   list[index]
+end
+
+def fetch_articles
+  [
+    { 
+      lines: [
+        { 
+          words: [
+            {
+              id: "asdkjhg",
+              image: "http://localhost:9001/word/asdkjhg.jpg"
+            },          
+            {
+              id: "00001",
+              image: "http://localhost:9001/word/00001.jpg"
+            }
+
+          ]
+        }
+      ]
+    }
+  ]
 end
