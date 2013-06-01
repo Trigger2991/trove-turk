0f1b09a8f91e40942a71b790fc1ccb3be23ea6f1
Adam Smith <adam.mackenzie.smith@gmail.com>
2013-06-01 16:50:01 +1000

    added initial question service

--- guess-question/main.rb
+++ guess-question/main.rb
@@ -1,7 +1,32 @@
 require 'sinatra'
+require 'json'
+
+set :port, 9002
 
 get '/question.json' do
+    cache_control :no_cache
+    content_type "application/json", :charset => 'utf-8'
+    articles = [
+      { 
+        lines: [
+          { 
+            words: [
+              {
+                id: "asdkjhg",
+                image: "http://localhost:9001/word/asdkjhg.jpg"
+              }
+            ]
+          }
+        ]
+      }
+    ]
     
-    'Insert question here'
 
+    word = articles.first[:lines].first[:words].first
+
+    question = {
+      id: word[:id],
+      image: word[:image]
+    }
+    question.to_json
 end
