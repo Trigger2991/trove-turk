7cceeec8614eddc92a3d16248e1e0ac4250bcf44
James Gregory <james@jagregory.com>
2013-06-02 12:52:23 +1000

    Add basic captcha form

---
+++ captcha/main.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'haml'
+require 'sass'
+
+set :port, 9006
+
+get '/' do
+  haml File.read('./index.html.haml')
+end
+
+get '/index.css' do
+  sass File.read('./index.css.sass')
+end
+
+get /(.*.js)/ do |name|
+  send_file "./public#{name}"
+end
+
