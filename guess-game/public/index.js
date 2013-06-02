96d1c5c7c34931e05eaeeb7744c9a8cc6b4a8d5c
James Gregory <james@jagregory.com>
2013-06-02 14:55:09 +1000

    Q and A service hostnames

--- guess-game/public/index.js
+++ guess-game/public/index.js
@@ -1,14 +1,17 @@
+var question_service = 'localhost:9002'
+var answer_service = 'localhost:9004'
+
 (function() {
   (function fetch_initial_questions() {
     $.get(
-      'http://localhost:9002/question?t=' + (+new Date()),
+      'http://' + question_service + '/question?t=' + (+new Date()),
       function(data) {
         $('#id').val(data["id"]);
         $('#image1 > img').attr('src', data["image"])
       }
     );
     $.get(
-      'http://localhost:9002/question?t=' + (+new Date()),
+      'http://' + question_service + '/question?t=' + (+new Date()),
       function(data) {
         $('#image2 > img').attr('src', data["image"]);
         $('#idholder').text(data["id"]);
@@ -22,7 +25,7 @@
 
     $('#guess').submit(function() {
       $.ajax({
-        url: 'http://localhost:9004/answers/new',
+        url: 'http://' + answer_service + '/answers/new',
         type: 'post',
         contentType: 'application/json; charset=utf-8',
         success: function(result) {
@@ -39,7 +42,7 @@
       });
 
       $.get(
-        'http://localhost:9002/question',
+        'http://' + question_service + '/question',
         function(data) {
           set_data(data);
         }
